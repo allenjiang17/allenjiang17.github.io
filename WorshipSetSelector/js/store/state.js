43905b4fc2658b53e49c7f799584ce4126bb8abd
Yi Chen <yi.chen901@gmail.com>
2022-02-03 15:29:01 -0500

    :memo: edited default lyrics

--- WorshipSetSelector/js/store/state.js
+++ WorshipSetSelector/js/store/state.js
@@ -2,26 +2,26 @@ export default {
     songs: [
         {title: 'Set a Fire',
             lyrics: `No place I would rather be
-                \nNo place I would rather be
-                \nNo place I would rather be
-                \nThen here in Your love, here in Your love
-                \n\nSet a fire down in my soul
-                \nThat I can't contain and I can't control
-                \nI want more of You God
-                \nI want more of You God
+                No place I would rather be
+                No place I would rather be
+                Then here in Your love, here in Your love
+                \nSet a fire down in my soul
+                That I can't contain and I can't control
+                I want more of You God
+                I want more of You God
             `},
         {title: 'Not in a Hurry',
             lyrics: `Lord I don't want to rush on ahead
-                \nIn my own strength
-                \nWhen You're right here
-                \n\nI'm not in a hurry
-                \nWhen it comes to Your spirit
-                \nWhen it comes to Your presence
-                \nWhen it comes to Your voice
-                \n\nI'm learning to listen
-                \nJust to rest in Your nearness
-                \nI'm starting to notice
-                \nYou are speaking
+                In my own strength
+                When You're right here
+                \nI'm not in a hurry
+                When it comes to Your spirit
+                When it comes to Your presence
+                When it comes to Your voice
+                \nI'm learning to listen
+                Just to rest in Your nearness
+                I'm starting to notice
+                You are speaking
             `}
     ],
     currsong: 0,
