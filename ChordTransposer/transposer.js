3836ccb16a0dfbcd325ddce8a724acf6d4b7903c
Allen Jiang <allenjiang17@gmail.com>
2022-12-15 11:29:46 -0500

    working version 1.0

--- ChordTransposer/transposer.js
+++ ChordTransposer/transposer.js
@@ -1,4 +1,3 @@
-
 /*
 Index code: 
 0 = A
@@ -15,26 +14,137 @@ Index code:
 11 = G#/Ab  - Ab preferred
 */
 
+//Global Variables
+var KEY;
+
 const chord_vals = [["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"],
               ["A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab"]];
 
-//True = sharp; false = flat
+//true = sharp; false = flat
+const key_natures = {0: true,
+               1: false,
+               2: true,
+               3: true,
+               4: false,
+               5: true,
+               6: false,
+               7: true,
+               8: false,
+               9: true,
+               10: true,
+               11: false};
 
-const key_natures = {0: True,
-               1: False,
-               2: True,
-               3: True,
-               4: False,
-               5: True,
-               6: False,
-               7: True,
-               8: False,
-               9: True,
-               10: True,
-               11: False};
+document.getElementById("text_entry").addEventListener("input", updateKey);
+document.getElementById("button_plus").addEventListener("click", function(){transposeText(+1)});
+document.getElementById("button_minus").addEventListener("click", function(){transposeText(-1)});
 
+
+function updateKey() {
+    let text = document.getElementById("text_entry").value;
+    KEY = determine_key(text);
+
+    //update key on html
+    if (key_natures[KEY]) {
+        document.getElementById("key_display").innerText = chord_vals[0][KEY];
+    
+    } else {
+        document.getElementById("key_display").innerText = chord_vals[1][KEY];
+    }
+}
+
+function transposeText(num_steps) {
+    let text = document.getElementById("text_entry").value;
+    document.getElementById("text_entry").value = transpose(text, num_steps);
+    updateKey();
+
+}
+
+function determine_key(raw_text) {
+    var text = raw_text.split("\n");
+
+    var total_chords = [];
+
+    for (let i=0; i<text.length; i++) {
+        //determine whether the line is lyric or chord
+        if (chord_line(text[i])) {
+            var chords = "";
+
+            //find the chords in a line
+            for (let j=0; j<text[i].length; j++) {
+                if ((/[a-zA-Z1-9#\/]/).test(text[i][j])) {
+                    chords = chords.concat(text[i][j]);
+                } else {
+
+                    if (chords.length != 0) {
+                        total_chords.push(chords);
+                        chords = "";
+                    }
+                }
+            }
+            //add last chord at end of line if it exists
+            if (chords.length != 0) {
+                total_chords.push(chords);
+            }
+
+            
+        }
+    }
+    return infer_key(total_chords);
+
+}
+function transpose(raw_text, num_steps) {    
+
+    var text = raw_text.split("\n");
+    var new_text = "";
+
+    for (let i=0; i<text.length; i++) {
+        //determine whether the line is lyric or chord
+        if (chord_line(text[i])) {
+            var chord = "";
+            var new_line = text[i];
+
+            //find the chords in a line
+            for (let j=0; j<text[i].length; j++) {
+
+                if ((/[a-zA-Z1-9#]/).test(text[i][j])) {
+                    chord = chord.concat(text[i][j]);
+                } else {
+                    if (chord.length != 0) {
+
+                        var new_chord = transpose_chord(chord, num_steps); 
+                        //to lower case prevents repeat replacing later on
+                        new_line = new_line.replace(chord, new_chord.toLowerCase());
+                        chord = "";
+
+                    }
+                }
+            }
+            //add last chord at end of line if it exists
+            if (chord.length != 0) {
+                var new_chord = transpose_chord(chord, num_steps); 
+                new_line = new_line.replace(chord, new_chord.toLowerCase());
+            }
+
+            //corrections
+            new_line = new_line.toUpperCase();
+            new_line = new_line.replaceAll("M","m");
+            new_line = new_line.replaceAll("AB","Ab");
+            new_line = new_line.replaceAll("BB","Bb");
+            new_line = new_line.replaceAll("DB","Db");
+            new_line = new_line.replaceAll("EB","Eb");
+            new_line = new_line.replaceAll("GB","Gb");
+
+            new_text = new_text + new_line + "\n";
+
+        } else {
+            new_text = new_text + text[i] + "\n";
+        }
+    }
+    return new_text
+}
 
 //determines whether a line of text is a chord line or not
+//return true or false
 function chord_line(line) {
     /*combination of several factors:
     #1. presence of excluded characters (less likely to be chords)
@@ -51,190 +161,252 @@ function chord_line(line) {
     var excluded_count = 0;
     var chord_count = 0;
 
-    for (let i=0; i<line.len; i++) {
+    for (let i=0; i<line.length; i++) {
     
         //spaces
-        if (line[i].isspace()) {
+        if (line[i] == " ") {
             space_count += 1;
-        } else {
+        } else if ((/[a-zA-Z1-9]/).test(line[i])) {
             letter_count += 1;
         }
 
         //upper case
-        if (line[i].isupper()) {
+        if (line[i] = line[i].toUpperCase()) {
             upper_count += 1;
-        } else if (line[i].islower()) {
+        } else if (line[i] = line[i].toLowerCase()) {
             lower_count += 1;
         }
 
         //excluded characters
-        if (excluded_characters.includes(line[i].lower())){
+        if (excluded_characters.includes(line[i].toLowerCase())){
             excluded_count += 1;
-        } else if (chord_characters.includes(line[i].lower())) {
+        } else if (chord_characters.includes(line[i].toLowerCase())) {
             chord_count += 1;
          }
 
+    }
+
+    if (letter_count == 0) {
+        return false;
     }
 
     //heuristic formula for determining likelihood of chord line
     var score = (upper_count - lower_count) + (space_count - letter_count) - 4 * excluded_count + chord_count;
 
     if (score >= -2) {
-        return True;
+        return true;
     } else {
-        return False
+        return false
     }
 
 }
 
+//sanitizes and determines the integer value mapping of the chord
+//returns a tuple (integer value, chord match)
+function chord_to_val(chord) {
+
+    var val;
+    var chord_match;
+    var max_chord_length = 0;
+
+    //iterate through chords and see if any match. chord_vals[0] refers to sharp chords, chord_vals[1] flat chords
+    for (let i=0; i<chord_vals[0].length; i++) {
+        if (chord.includes(chord_vals[0][i])) {
+            if (chord_vals[0][i].length > max_chord_length) {
+                max_chord_length = chord_vals[0][i].length;
+                chord_match = chord_vals[0][i];
+                val = i;
+            }      
+        }
+        if (chord.includes(chord_vals[1][i])) {
+            if (chord_vals[1][i].length > max_chord_length) {
+                max_chord_length = chord_vals[1][i].length;
+                chord_match = chord_vals[1][i];
+                val = i;
+
+            }  
+        }
+    }
+
+    //if no chord match, error
+    if (max_chord_length == 0) {
+        console.log("An error occurred. Chord could not be matched:", chord);
+        return null, chord;
+    }
+    return [val, chord_match]
+}
+
+//determines new value after transposing (wrapping around)
+function val_transpose(val, num_steps) {
+    var new_val;
+
+    if (num_steps > 0) {
+        new_val = ((val + num_steps) % 12);
+    } else {
+        new_val = (val + num_steps);
+        if (new_val < 0) {
+            new_val += 12;
+        }
+    }
+    return new_val;
+}
+
 /*
-#sanitizes and determines the integer value mapping of the chord
-def chord_to_val(chord):
-    chord_matches = []
+function transpose_chord(input_chord, num_steps) {
+    if (input_chord.includes("/")) {
+        var split_chords = input_chord.split("/");
+        return (transpose_chord_piece(split_chords[0], num_steps) + "/" + transpose_chord_piece(split_chords[1], num_steps));
 
-    # iterate through chords and see if any match
-    for val, chord_match in enumerate(chord_vals[0]):
-        if chord_match in chord:
-            #chord_matches.append((val, chord_match, len(chord_match.decode("utf-8")))) for python 2
-            chord_matches.append((val, chord_match, len(chord_match)))
+    } else {
+        return (transpose_chord_piece(input_chord, num_steps));
+    }
 
-    for val, chord_match in enumerate(chord_vals[1]):
-        if chord_match in chord:
-            chord_matches.append((val, chord_match, len(chord_match)))
+}
+*/
 
-    #if no chord match, raise problem
-    if not chord_matches:
-        print("Chord could not be matched:", chord)
-        return None, chord
-    # find best chord match (longest one)
-    val = max(chord_matches, key=lambda i: i[2])[0]
-    chord_match = max(chord_matches, key=lambda i: i[2])[1]
+//transposes chord using input chord and number of steps
+function transpose_chord(input_chord, num_steps) {
 
-    return val, chord_match
+    var chord_match_array = chord_to_val(input_chord);
+    var val = chord_match_array[0];
+    var chord_match = chord_match_array[1]; 
 
-#determines new value after transposing (wrapping around)
-def val_transpose(val, num_steps):
+    if (val == null) {
+        return chord_match;
+    }
+        
+    var new_val = val_transpose(val, num_steps);
 
-    if num_steps > 0:
-        new_val = ((val + num_steps) % 12)
-    else:
-        new_val = (val + num_steps)
-        if new_val < 0:
-            new_val += 12
+    if (key_natures[val_transpose(KEY, num_steps)]) {
+        return input_chord.replace(chord_match, chord_vals[0][new_val]);
+    } else {
+        return input_chord.replace(chord_match, chord_vals[1][new_val]);
 
-    return new_val
+    }
+}
 
-#transposes chord using input chord and number of steps
-def transpose_chord(input_chord, num_steps, sharp):
+//utility function, counts to see the most common item in an array
+function counter(input_array) {
+    var return_array = [];
 
-    print("Chord: ", input_chord)
+    for (let i=0; i<input_array.length; i++) {
+        //check if input is already in the return array
+        if (!(JSON.stringify(return_array).includes(JSON.stringify(input_array[i])))) { 
+            
+            var count = 0;
 
-    new_chord = input_chord
-    chords = input_chord.split("/")
+            for (let j=0; j<input_array.length; j++) {
+                if (JSON.stringify(input_array[i]) == JSON.stringify(input_array[j])) {
+                    count++;
+                }                
+            }
+            return_array.push([input_array[i], count]);
 
-    for chord in chords:
+        }
+    }
+    return return_array;
+}
 
-        val, chord_match = chord_to_val(chord)
-        if val is None:
-            return chord_match
+function infer_key(chords) {
+    var val_list = [];
+    var minor;
+    var prob;
+    //list of tuples containing (integer val of chord, minor/major bool)
 
-        new_val = val_transpose(val, num_steps)
+    for (let i=0; i<chords.length; i++ ){
 
-        if sharp:
-            #lowercase version of the chord, as a mark to indicate it has been replaced, preventing duplicate replacement
-            new_chord = new_chord.replace(chord_match, chord_vals[0][new_val].lower())
+        //take first part of chord, bass not does not matter
+        var raw_chord = chords[i].split("/")[0];
+        var chord_match_array = chord_to_val(raw_chord);
+        var val = chord_match_array[0];
+        var chord_match = chord_match_array[1];
 
-        else:
-            new_chord = new_chord.replace(chord_match, chord_vals[1][new_val].lower())
+
+        if (val == null) {
+            continue;
+        }
+
+        if (raw_chord.includes("m")) {
+            minor = true;
+        
+        } else {
+            minor = false;
+        }
 
 
-    print("Transposed Chord: ", new_chord)
-    return new_chord
+        val_list.push([val, minor]);
 
-def infer_key(chords):
-    val_list = []
-    #list of tuples containing (integer val of chord, minor/major bool)
+    }
 
-    for chord in chords:
+    var counted_val_list = counter(val_list);
+    var final_list = [];
 
-        chord = chord.split("/")[0]
-        val, chord_match = chord_to_val(chord)
+    for (let i=0; i<counted_val_list.length; i++) {
+        var val = counted_val_list[i];
 
-        if val is None:
-            continue
+        prob = val[1]; //add to probability the frequency of the chord appearence itself
 
-        if "m" in chord_match:
-            minor = True
-        else:
-            minor = False
+        //if the relative IV, V and vii of each key is present, more likely it is in that key
+        var fourth = (val[0][0] + 5) % 12;
+        var fifth = (val[0][0] + 7) % 12;
+        var sixth = (val[0][0] + 9) % 12;
 
-        val_list.append((val, minor))
+        for (let j=0; j<counted_val_list.length; j++) {
+            //add the frequency of the chord appearance to create a crude "probability"
+            var val2 = counted_val_list[j];
 
-    print(val_list)
-    #make a list of the frequency of each chord appearing
-    counted_val_list = Counter(val_list).most_common()
-    final_list = []
+            if (val2[0][0] == fourth && !val2[0][1]) {
+                prob += val2[1];
+            }
+            else if (val2[0][0] == fifth && !val2[0][1]) {
+                prob += val2[1];
+            }
+            else if (val2[0][0] == sixth && val2[0][1]) {
+                prob += val2[1];
+            }
+        }
+        final_list.push([val[0][0], prob]);
 
-    for val in counted_val_list:
-        prob = val[1] #add to probability the frequency of the chord appearence itself
+    }
 
-        #if the relative IV, V and vii of each key is present, more likely it is in that key
-        fourth = (val[0][0] + 5) % 12
-        fifth = (val[0][0] + 7) % 12
-        sixth = (val[0][0] + 9) % 12
+    //sort the list based on probabilities, find highest probability
+    var sorted_final_list =  final_list.sort(function(a, b){return b[1] - a[1]}); 
 
-        for val2 in counted_val_list:
+    //If the list is not empty
+    if (sorted_final_list.length != 0) {
 
-            #add the frequency of the chord appearance to create a crude "probability"
-            if val2[0][0] == fourth and not val2[0][1]:
-                prob += val2[1]
-            elif val2[0][0] == fifth and not val2[0][1]:
-                prob += val2[1]
-            elif val2[0][0] == sixth and val2[0][1]:
-                prob += val2[1]
+        return sorted_final_list[0][0];
 
-        final_list.append((val[0][0], prob))
+    } else {
+        console.log("Key could not be determined");
+        return 0;
 
-    #sort the list based on probabilities, find highest probability
-    sorted_final_list = sorted(final_list, key = lambda x : x[1], reverse=True)
+    }
 
-    print(sorted_final_list)
+}
 
-    #If the list is not empty
-    if sorted_final_list:
+function increment_key(key, direction) {
+    var val = chord_to_val(key)[0];
 
-        #return highest probability chord as the key
-        if key_nature(sorted_final_list[0][0]):
-            return chord_vals[0][sorted_final_list[0][0]]
-        else:
-            return chord_vals[1][sorted_final_list[0][0]]
+    if (direction == "UP") {
+        new_val = val_transpose(val, 1);
+    }
+    else {
+        new_val = val_transpose(val, -1);
+    }
 
-    else:
-        return ""
+    if (key_natures[new_val]) {
+        return chord_vals[0][new_val];
+    }
+    else {
+        return chord_vals[1][new_val];
+    }
 
-def key_nature(val):
+}
 
-    if type(val) != int:
-        val, _ = chord_to_val(val)
-
-    return key_natures.get(val, True) #default set to sharp
-
-def increment_key(key, direction):
-    val, _ = chord_to_val(key)
-
-    if direction == "UP":
-        new_val = val_transpose(val, 1)
-    else:
-        new_val = val_transpose(val, -1)
-
-    if key_nature(new_val):
-        return chord_vals[0][new_val]
-    else:
-        return chord_vals[1][new_val]
-
-def key_difference(key1, key2):
-    val1, _ = chord_to_val(key1)
-    val2, _ = chord_to_val(key2)
+function key_difference(key1, key2) {
+    var val1 = chord_to_val(key1)[0];
+    var val2 = chord_to_val(key2)[0];
 
     return val2 - val1
-*/
+}
