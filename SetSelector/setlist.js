754f52c27fb75b6f862527a50018bca27fc6fa49
Allen Jiang <allenjiang17@gmail.com>
2023-01-08 15:05:58 -0500

    added basic export for Set selector

--- SetSelector/setlist.js
+++ SetSelector/setlist.js
@@ -53,3 +53,36 @@ function refreshCurrentSong() {
 function deleteSetSong() {
     this.parentElement.remove();
 }
+
+//from https://robkendal.co.uk/blog/2020-04-17-saving-text-to-client-side-file-using-vanilla-js
+const downloadToFile = (content, filename, contentType) => {
+    const a = document.createElement('a');
+    const file = new Blob([content], {type: contentType});
+    
+    a.href= URL.createObjectURL(file);
+    a.download = filename;
+    a.click();
+  
+      URL.revokeObjectURL(a.href);
+};
+
+
+function downloadSet() {
+    var set_list = document.querySelector('#set_list_items');
+    var list_of_songs = set_list.getElementsByTagName("li");
+    var export_string = "";
+
+    for (let i=0; i<list_of_songs.length; i++) {
+        
+        export_string = export_string + "\n" + list_of_songs[i].firstChild.getAttribute("data-sheet");
+
+        
+    }
+    today = new Date();
+    var date = String(today.getMonth() + 1).padStart(2, '0') + String(today.getDate()).padStart(2, '0') + String(today.getFullYear()).substring(2);
+    downloadToFile(export_string, 'set' + date + '.txt', 'text/plain');
+
+}
+
+document.querySelector('#export_button').addEventListener('click', downloadSet);
+
