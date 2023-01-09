2b92d9f686e56315d7bc7c66aeb703769fd0b0da
Allen Jiang <allenjiang17@gmail.com>
2023-01-09 13:27:48 -0500

    added draggable

--- SetSelector/setlist.js
+++ SetSelector/setlist.js
@@ -15,7 +15,7 @@ function addSongToSet() {
     newEntry.setAttribute("data-key", KEY);
     newEntry.setAttribute("data-sheet", document.getElementById("text_entry").value);
     newEntry.className = "set_list_item";
-    newEntry.addEventListener("click", refreshCurrentSong);
+    newEntry.addEventListener("click", selectCurrentSongFromClick);
 
     newTitle.setAttribute("id", "set_title" + current_list.length);
     newTitle.className = "set_title";
@@ -29,59 +29,32 @@ function addSongToSet() {
     newEntry.appendChild(newTitle);
     newEntry.appendChild(newButton);
     set_list.append(newEntry);
+
+    makeDraggableList();
 }
 
-function refreshCurrentSong() {
-    currElement = this;
+function selectCurrentSong(targetElement) {
 
     //remove highlights from other songs
-    var list_of_songs = currElement.parentElement.getElementsByTagName("li");
+    var list_of_songs = targetElement.parentElement.getElementsByTagName("li");
     
     for (let i=0; i<list_of_songs.length; i++) {
-        list_of_songs[i].style.backgroundColor = "#f6f6f6";
-        list_of_songs[i].firstChild.style.color = "black";
-        list_of_songs[i].firstChild.style.fontWeight = "normal"
+        list_of_songs[i].classList.remove("selected");
     }
 
     //TODO: if button is clicked, don't do the following for redundancy
-    CURRENT_SONG_ID = currElement.getAttribute("data-id");
-    CURRENT_SET_SONG_NO = currElement.getAttribute("data-song-no");
-    document.getElementById("text_entry").value = currElement.getAttribute("data-sheet");
+    CURRENT_SONG_ID = targetElement.getAttribute("data-id");
+    CURRENT_SET_SONG_NO = targetElement.getAttribute("data-song-no");
+    document.getElementById("text_entry").value = targetElement.getAttribute("data-sheet");
     updateKey();
 
     //change style to indicate selected song
-    currElement.style.backgroundColor = "rgb(52, 86, 139)";
-    currElement.firstChild.style.color = "white";
-    currElement.firstChild.style.fontWeight = "bold";
+    targetElement.classList.add("selected");
 }
 
-//TODO: combine both functions somehow
-function refreshCurrentSongFromArrowKeys(set_item_id) {
-
-   var currElement = document.getElementById(set_item_id);
-   if (currElement === undefined || currElement === null) {
-        return;
-   }
-
-    //remove highlights from other songs
-    var list_of_songs = currElement.parentElement.getElementsByTagName("li");
-    
-    for (let i=0; i<list_of_songs.length; i++) {
-        list_of_songs[i].style.backgroundColor = "#f6f6f6";
-        list_of_songs[i].firstChild.style.color = "black";
-        list_of_songs[i].firstChild.style.fontWeight = "normal"
-    }
-
-    //TODO: if button is clicked, don't do the following for redundancy
-    CURRENT_SONG_ID = currElement.getAttribute("data-id");
-    CURRENT_SET_SONG_NO = currElement.getAttribute("data-song-no");
-    document.getElementById("text_entry").value = currElement.getAttribute("data-sheet");
-    updateKey();
-
-    //change style to indicate selected song
-    currElement.style.backgroundColor = "rgb(52, 86, 139)";
-    currElement.firstChild.style.color = "white";
-    currElement.firstChild.style.fontWeight = "bold";
+//necessary because the default parameter passed into an event handing function is the event, not the target object, which is referenced by "this"
+function selectCurrentSongFromClick() {
+    selectCurrentSong(this);
 }
     
 function deleteSetSong() {
@@ -134,11 +107,70 @@ function checkKey(e) {
             e.preventDefault();
 
         } 
-        refreshCurrentSongFromArrowKeys(set_item_id);
+        selectCurrentSong(document.getElementById(set_item_id));
 
     }   
 }
+//from: https://code-boxx.com/drag-drop-sortable-list-javascript/
+function makeDraggableList () {
+    
+    // (A) SET CSS + GET ALL LIST ITEMS
+    var set_list = document.getElementById("set_list_items")
+    let set_list_items = set_list.getElementsByTagName("li")
+    let current = null;
+  
+    // (B) MAKE ITEMS DRAGGABLE + SORTABLE
+    for (let i of set_list_items) {
+      // (B1) ATTACH DRAGGABLE
+      i.draggable = true;
+      
+      // (B2) DRAG START - YELLOW HIGHLIGHT DROPZONES
+      i.ondragstart = (ev) => {
+        current = i;
+        for (let it of set_list_items) {
+          if (it != current) { it.classList.add("hint"); }
+        }
+      };
+      
+      // (B3) DRAG ENTER - RED HIGHLIGHT DROPZONE
+      i.ondragenter = (ev) => {
+        if (i != current) { i.classList.add("active"); }
+      };
+  
+      // (B4) DRAG LEAVE - REMOVE RED HIGHLIGHT
+      i.ondragleave = () => {
+        i.classList.remove("active");
+      };
+  
+      // (B5) DRAG END - REMOVE ALL HIGHLIGHTS
+      i.ondragend = () => { for (let it of set_list_items) {
+          it.classList.remove("hint");
+          it.classList.remove("active");
+      }};
+   
+      // (B6) DRAG OVER - PREVENT THE DEFAULT "DROP", SO WE CAN DO OUR OWN
+      i.ondragover = (evt) => { evt.preventDefault(); };
+   
+      // (B7) ON DROP - DO SOMETHING
+      i.ondrop = (evt) => {
+        evt.preventDefault();
+        if (i != current) {
+          let currentpos = 0, droppedpos = 0;
+          for (let it=0; it<set_list_items.length; it++) {
+            if (current == set_list_items[it]) { currentpos = it; }
+            if (i == set_list_items[it]) { droppedpos = it; }
+          }
+          if (currentpos < droppedpos) {
+            i.parentNode.insertBefore(current, i.nextSibling);
+          } else {
+            i.parentNode.insertBefore(current, i);
+          }
+        }
+      };
+    }
+  }
 
 
 document.querySelector('#export_button').addEventListener('click', downloadSet);
 document.onkeydown = checkKey;
+
