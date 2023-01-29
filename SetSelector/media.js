eab75f001092f2d23dd0a601ec5bbf5a075723ac
Yi Chen <yi.chen901@gmail.com>
2023-01-29 18:02:41 -0500

    media mode transition

--- SetSelector/media.js
+++ SetSelector/media.js
@@ -1,15 +1,30 @@
 function switchMedia() {
   const chords = document.getElementById("chord_display")
   const media = document.getElementById("media_display")
+  const transition = document.getElementById('transition_display')
+  const transition2 = document.getElementById('fakepresentation')
+
   if (media.style.display == "block") {
-    chords.style.display = "block"
     media.style.display = "none"
-    document.getElementById('media_button').src = "icons/projector-fill.svg"
-    document.getElementById("media_button_text").innerHTML = "Switch to media mode"
+    transition.style.opacity = "1"
+    transition.classList.toggle('media')
+    transition2.classList.toggle('media')
+    window.setTimeout(function () {
+      transition.style.opacity = "0"
+      chords.style.display = "block"
+      document.getElementById('media_button').src = "icons/projector-fill.svg"
+      document.getElementById("media_button_text").innerHTML = "Switch to media mode"
+    }, 700);
   } else {
     chords.style.display = "none"
-    media.style.display = "block"
-    document.getElementById('media_button').src = "icons/music-note-list.svg"
-    document.getElementById("media_button_text").innerHTML = "Switch to chord mode"
+    transition.style.opacity = "1"
+    transition.classList.toggle('media')
+    transition2.classList.toggle('media')
+    window.setTimeout(function () {
+      transition.style.opacity = "0"
+      media.style.display = "block"
+      document.getElementById('media_button').src = "icons/music-note-list.svg"
+      document.getElementById("media_button_text").innerHTML = "Switch to chord mode"
+    }, 700);
   }
 }
