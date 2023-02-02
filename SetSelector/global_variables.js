d71866776c6905930e3ff4534a8ac566b871b3a0
Yi Chen <yi.chen901@gmail.com>
2023-02-02 16:34:02 -0500

    change screen hidden to uppercase

--- SetSelector/global_variables.js
+++ SetSelector/global_variables.js
@@ -1,7 +1,6 @@
 var CURRENT_SONG_ID = -1;
 var CURRENT_SET_SONG_NO=-1;
 var CURRENT_LYRIC=-1;
-var SCREEN_HIDDEN = false;
 
 //transposer_related
 var KEY_INDEX;
@@ -9,4 +8,4 @@ var KEY;
 
 //media
 var presentwindow = null;
-var screenBlack = false;
+var SCREEN_HIDDEN = false;
