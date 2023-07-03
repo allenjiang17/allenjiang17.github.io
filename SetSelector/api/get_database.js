b265b0e3bf6b6e926712d49e0af3fb5e98e67da3
Allen Jiang <allenjiang17@gmail.com>
2023-07-03 18:11:00 -0400

    cleaned up repo removed node.js stuff

--- SetSelector/api/get_database.js
+++
@@ -1,9 +0,0 @@
-import { db } from '@vercel/postgres';
- 
-export default async function handler(request, response) {
-  const client = await db.connect();
-  const DATABASE = await client.sql`SELECT * FROM Songs;`;
-
- return response.status(200).json({DATABASE});
-
-}
